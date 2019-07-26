46cb08e7682cf351d1a358dac742699d052ca20b
Kizmet <bryantpatton@gmail.com>
2019-07-26 09:47:07 -0700

    Part 1 Completed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Loader,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Card,
+  Image,
+  Button
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-export default function CharacterList() {
+const CharacterList = () => {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const url = "https://rickandmortyapi.com/api/character/";
 
   useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const result = await axios.get(url);
+      const { data } = await result;
+      setCharacters(data.results);
+      setIsLoading(false);
+    };
+    fetchData();
+    return () => console.log("The Effect Hook has been cleaned up.");
+
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+  }, [url]);
 
-  return <section className='character-list grid-view'>
+  return (
+    <Container>
+      <style>
+        {`
+      html, body {
+        background-color: #252839 !important;
+      }
+    }
+    `}
+      </style>
+      {isLoading && (
+        <Fragment>
+          <Loader active inline className="slow red" />
+        </Fragment>
+      )}
+      <Header as="h2" icon inverted textAlign="center">
+        {console.log(characters)}
+        <Icon name="grid layout" />
+        Characters
+        <Header.Subheader>Rick and Morty Show Characters</Header.Subheader>
+      </Header>
+      <Grid celled columns={2}>
+        <Card.Group itemsPerRow={2} centered>
+          {characters.map(character => (
+            <Card key={character.id}>
+              <Card.Content>
+                <Link to={`/doctor/${character.id}/${character.name}`}>
+                  <Image className="characterImg" src={character.image} />
+                </Link>
+                <Card.Header>{`${character.name}`}</Card.Header>
+              </Card.Content>
+              <Card.Content extra>
+                  <Button basic color="blue">
+                    Page
+                  </Button>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
 
-}
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group>
+      </Grid>
+    </Container>
+  );
+};
+
+export default CharacterList;
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Loader,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Card,
+  Image,
+  Button
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-export default function LocationsList() {
+const LocationsList = () => {
+  // TODO: Add useState to track data from useEffect
+  const [locations, setLocations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const url = "https://rickandmortyapi.com/api/location/";
 
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const result = await axios.get(url);
+      const { data } = await result;
+      setLocations(data.results);
+      setIsLoading(false);
+    };
+    fetchData();
+    return () => console.log("The Effect Hook has been cleaned up.");
+
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  }, [url]);
+
+  return (
+    <Container>
+      {isLoading && (
+        <Fragment>
+          <Loader active inline className="slow red" />
+        </Fragment>
+      )}
+      <Header as="h2" icon inverted textAlign="center">
+        Characters
+        <Header.Subheader>Rick and Morty Show Characters</Header.Subheader>
+      </Header>
+      
+      <Grid celled columns={2}>
+        <Card.Group itemsPerRow={2} centered>
+          {locations.map(location => (
+            <Card key={location.id}>
+              <Card.Content>
+              <Link to={`/doctor/${location.id}/${location.name}`}>
+                <Image floated="right">
+                  <Icon name="world" size="large" color="green" />
+                </Image>
+                </Link>
+                <Card.Header>{location.name}</Card.Header>
+                <Card.Meta>{location.type}</Card.Meta>
+                <Card.Description>{location.dimension}</Card.Description>
+              </Card.Content>
+
+            </Card>
+          ))}
+        </Card.Group>
+      </Grid>
+    </Container>
+  );
+};
+
+export default LocationsList;
